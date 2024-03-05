f0e2da2076853f83c9d64b1ef6459bea6e4ea369
human-d3v <=>
2024-03-05 07:28:22 -0700

    changed keymap for stata do files

--- after/plugin/stata_lsp/stataHandler.lua
+++ after/plugin/stata_lsp/stataHandler.lua
@@ -43,9 +43,9 @@ vim.api.nvim_create_autocmd("FileType", {
 	callback = function ()
 		vim.schedule(function ()
 			vim.keymap.set("n", "<leader>mp", [[:lua OpenBufferTerminalRepl('stata-mp')<CR>]], {noremap=true, buffer=true})
-			vim.keymap.set({"v","x"}, "<leader><leader>t", [[:lua SendToRepl(1)<CR>]], {noremap=true, buffer=true})
-			vim.keymap.set("n", "<leader><leader>t", [[:lua SendToRepl(0)<CR>]], {noremap=true, buffer=true})
-			vim.keymap.set("n", "<leader><leader>at", [[:lua SendToRepl(2)<CR>]], {noremap=true, buffer=true})
+			vim.keymap.set({"v","x"}, "<BSlash>d", [[:lua SendToRepl(1)<CR>]], {noremap=true, buffer=true})
+			vim.keymap.set("n", "<BSlash>d", [[:lua SendToRepl(0)<CR>]], {noremap=true, buffer=true})
+			vim.keymap.set("n", "<BSlash>aa", [[:lua SendToRepl(2)<CR>]], {noremap=true, buffer=true})
 		end)
 	end,
 })
