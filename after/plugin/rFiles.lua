03ff935e2e301f6f7ceb7082bf5a4e36e50d05b7
human-d3v <=>
2024-04-04 20:11:16 -0600

    Updated comment %in% operator for r files

--- after/plugin/rFiles.lua
+++ after/plugin/rFiles.lua
@@ -3,6 +3,15 @@ function ResizeAndMove(px)
 	vim.cmd('res -' .. px)
 end
 
+local function R_in_Pipe()
+	local line = vim.api.nvim_get_current_line()
+	if string.sub(line,1,1) == "#" then
+		return "in "
+	else
+		return " %in% "
+	end
+end
+
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = {"r", "rnoweb","rmd"},
 	callback = function ()
@@ -10,7 +19,7 @@ vim.api.nvim_create_autocmd("FileType", {
 			vim.keymap.set('t','<leader>mv', ":lua ResizeAndMove(80)<CR>", {buffer = true})
 			vim.keymap.set("i", ">", " %>% ", {buffer = true})
 			vim.keymap.set("i", ">>", ">", {buffer = true})
-			vim.keymap.set("i", "in ", "%in%", {buffer = true})
+			vim.keymap.set("i", "in ", R_in_Pipe(), {buffer = true})
 			vim.keymap.set("i", "`", "```{r}\n\n```", {buffer = true})
 			vim.keymap.set("i", "``", "`", {buffer = true})
 		end)
