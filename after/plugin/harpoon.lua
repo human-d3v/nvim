940c5519186be92e31ffcc771781c3f68bb5a1d0
human-d3v <brytop05@gmail.com>
2024-05-09 13:26:52 -0600

    updated Lazy config, and reconfigured harpoon, lsp, cmp, etc.

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,12 +1,17 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local harpoon = require("harpoon")
 
-vim.keymap.set('n', '<leader>a', mark.add_file)
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
 
---harpoon menu appears when you type ctrl + e
-vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
---with the menu onscreen, map functions for navigation
-	vim.keymap.set('n', '<C-h>',function() ui.nav_file(1) end) --h for first
-	vim.keymap.set('n', '<C-g>',function() ui.nav_file(2) end) --g for second
-	vim.keymap.set('n', '<C-m>',function() ui.nav_file(3) end) --m for third
-	vim.keymap.set('n', '<C-s>',function() ui.nav_file(4) end) --s for fourth
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end) --add file to end of ui list
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,77 +1,63 @@
-local lsp = require('lsp-zero').preset({})
-
-lsp.preset("recommended")
-
-lsp.ensure_installed({
-	'rust_analyzer',
-})
---fix undefined global vim
-lsp.nvim_workspace();
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({select = true}),
-	["<C Space>"] = cmp.mapping.complete(),
+require('mason').setup({})
+require('mason-lspconfig').setup({
+	handlers = {
+		function (server_name)
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			require('lspconfig')[server_name].setup({
+				capabilities = capabilities,
+			})
+		end	
+	},
 })
 
-cmp_mappings['<Tab>'] = nil
-cmp_mappings['<S-Tab>'] = nil
 
---adding codeium setup with cmp
--- cmp.setup({
--- 	snippet = {
--- 		expand = function (args)
--- 			require('luasnip').lsp_expand(args.body)
--- 		end
--- 	},
--- 	window = {
--- 		
--- 	},
--- 	mapping = cmp_mappings,
--- 	sources = cmp.config.sources({
--- 		{name = "codeium"},
--- 		{name = 'luasnip'}
--- 	})
--- })
-
-lsp.setup_nvim_cmp({
-	mapping = cmp_mappings
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function (args)
+		local bufnr = args.buf
+		local opts = {buffer = bufnr, remap = false}
+		vim.keymap.set("n","gd",function() vim.lsp.buf.definition() end, opts) --go to definition
+		vim.keymap.set('n','K',function() vim.lsp.buf.hover() end, opts) -- hover
+		vim.keymap.set('n','<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts) --view workspace
+		vim.keymap.set('n','<leader>vd', function() vim.diagnostic.open_foat() end, opts) --view diagnostic
+		vim.keymap.set('n','[d',function() vim.diagnostic.goto_next() end, opts)
+		vim.keymap.set('n',']d',function() vim.diagnostic.goto_prev() end, opts)
+		vim.keymap.set('n','<leader>vca', function() vim.lsp.buf.code_action() end, opts) --view code action
+		vim.keymap.set('n','<leader>vrn', function() vim.lsp.buf.rename() end, opts) --rename variables
+		vim.keymap.set('n','<leader>vrr', function() vim.lsp.buf.references() end, opts)
+		vim.keymap.set('i','<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+	end
 })
 
-lsp.set_preferences({
-	suggest_lsp_servers = false,
-	sign_icons = { 
-		error = "E",
-		warm = "W",
-		hint = "H",
-		info = "I",
+
+--specific for lsp support for neovim configuration
+--shoutout to @VonHeikemen
+--local function nvim_workspace(opts)
+local runtime_path = vim.split(package.path, ";")
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+require('lspconfig').lua_ls.setup({
+		settings = {
+			Lua = {
+				--Disable telemetry
+				telemtry = {enable = false},
+				runtime = {
+					--tell lsp which version of lua you're using 
+					version = 'LuaJIT',
+					path = runtime_path,
+				},
+				diagnostics = {
+					globals = { 'vim'}
+				},
+				workspace = {
+					checkThirdParty = false,
+					
+					library = {
+					--make server aware of nvim runtime files
+						vim.fn.expand('$VIMRUNTIME/lua'),
+						vim.fn.stdpath('config') .. '/lua'
+					}
+				}
+		}
 	}
 })
 
---on attach is really cool functionality, each time I'm on a buffer, this snippet of code lives only for the live of the buffer session. 
---so if I navigate to something that doesn't have a buffer, I can still use these mapped values, but if there's no lsp, vim will do it's best to
---try to fulfill the request. 
-
-lsp.on_attach(function(client,bufnr)
-	local opts = {buffer = bufnr, remap = false}
-
-	vim.keymap.set("n","gd",function() vim.lsp.buf.definition() end, opts) --go to definition
-	vim.keymap.set('n','K',function() vim.lsp.buf.hover() end, opts) 
-	vim.keymap.set('n','<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts) --view workspace
-	vim.keymap.set('n','<leader>vd', function() vim.diagnostic.open_foat() end, opts)
-	vim.keymap.set('n','[d',function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set('n',']d',function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set('n','<leader>vca', function() vim.lsp.buf.code_action() end, opts) --view code action
-	vim.keymap.set('n','<leader>vrn', function() vim.lsp.buf.rename() end, opts) --rename variables
-	vim.keymap.set('n','<leader>vrr', function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set('i','<C-h>', function() vim.lsp.buf.signature_help() end, opts)
-end)
-
-lsp.setup()
-
-vim.diagnostic.config({
-	virtual_text=true
-})
