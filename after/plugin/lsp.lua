a89b5894956de5cdd2e92aacfd048cdb84648f3b
human-d3v <brytop05@gmail.com>
2024-05-10 12:52:55 -0600

    Fixed open_foat to open_float

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -14,11 +14,11 @@ require('mason-lspconfig').setup({
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function (args)
 		local bufnr = args.buf
-		local opts = {buf = bufnr, remap = false}
+		local opts = {buffer = bufnr, remap = false}
 		vim.keymap.set("n","gd",function() vim.lsp.buf.definition() end, opts) --go to definition
 		vim.keymap.set('n','K',function() vim.lsp.buf.hover() end, opts) -- hover
 		vim.keymap.set('n','<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts) --view workspace
-		vim.keymap.set('n','<leader>vd', function() vim.diagnostic.open_foat() end, opts) --view diagnostic
+		vim.keymap.set('n','<leader>vd', function() vim.diagnostic.open_float() end, opts) --view diagnostic
 		vim.keymap.set('n','[d',function() vim.diagnostic.goto_next() end, opts)
 		vim.keymap.set('n',']d',function() vim.diagnostic.goto_prev() end, opts)
 		vim.keymap.set('n','<leader>vca', function() vim.lsp.buf.code_action() end, opts) --view code action
