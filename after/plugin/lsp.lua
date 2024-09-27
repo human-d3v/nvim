ae79f798d88b0acfc5bf9958707c172d6334de13
human-d3v <BryTop05@gmail.com>
2024-09-27 10:33:12 -0600

    Removed comment autocreation on newline

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,7 +10,6 @@ require('mason-lspconfig').setup({
 	},
 })
 
-
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function (args)
 		local bufnr = args.buf
@@ -27,6 +26,15 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set('n','<leader>h', function() vim.lsp.buf.signature_help() end, opts)
 		vim.keymap.set('n', '<leader>ch', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, opts)
 	end
+})
+
+-- remove comment line on newline
+vim.api.nvim_create_autocmd("BufEnter", {
+	callback= function()
+		vim.opt.formatoptions:remove {"c", "r", "o"}
+	end,
+	group=general, 
+	desc = "Disable new line comment"
 })
 
 local dap = require('dap')
