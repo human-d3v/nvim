749d30744f2707bcc1572e2371e8305aa8195d85
human-d3v <BryTop05@gmail.com>
2024-09-27 10:48:10 -0600

    updated the stata handler and the r_files remap. for the %>% dplyr pipe

--- after/plugin/stata_lsp/stataHandler.lua
+++ after/plugin/stata_lsp/stataHandler.lua
@@ -8,7 +8,7 @@ vim.api.nvim_create_autocmd("FileType", {
 			vim.keymap.set("n", "<BSlash>aa", [[:lua SendToRepl('stata',2)<CR>]], {noremap=true, buffer=true})
 			vim.keymap.set("n", "<Bslash>q", [[:lua VerifyCloseBuffer('stata')<CR>]], {noremap=true, buffer=true})
 			vim.keymap.set('n', '<Bslash>rm', [[:lua SendToRepl('stata', 3, "exit")<CR>]], {noremap=true, buffer=true})
-			vim.keymap.set('n', '<Bslash>n', [[:lua SendToRepl('stata', 3, "stata-mp")<CR>]], {noremap=true, buffer=true})
+			vim.keymap.set('n', '<Bslash>n', [[:lua SendToRepl('stata', 3, "clear\nexit\nstata-mp")<CR>]], {noremap=true, buffer=true})
 		end)
 	end,
 })
