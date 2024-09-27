b22e751ceb4bdc6a293bbca177bb10d9f8660790
human-d3v <BryTop05@gmail.com>
2024-09-27 10:50:20 -0600

    Updated dplyr pipe operator

--- after/plugin/rFiles.lua
+++ after/plugin/rFiles.lua
@@ -3,8 +3,7 @@ vim.api.nvim_create_autocmd("FileType", {
 	callback = function ()
 		vim.schedule(function ()
 			vim.keymap.set('t','<leader>mv', ":lua ResizeAndMove(80)<CR>", {buffer = true})
-			vim.keymap.set("i", ">", " %>% ", {buffer = true})
-			vim.keymap.set("i", ">>", ">", {buffer = true})
+			vim.keymap.set("i", "<C-.>", " %>% ", {buffer = true})
 			vim.keymap.set("i", "<C-->", "<Plug>RAssign", {buffer = true, silent = true}) -- reassign keymap for assignment
 			require('cmp').setup({sources = {{ name = "cmp_r" }}})
 			require('cmp_r').setup({})
