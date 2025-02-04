601b11f5fb8e5e123ce62b6bd99094baecf37c5c
human-d3v <brytop05@gmail.com>
2025-02-04 08:09:19 -0700

    Moved font size to nested os checker

--- after/plugin/neovide.lua
+++ after/plugin/neovide.lua
@@ -1,9 +1,9 @@
 if vim.g.neovide then 
-	vim.o.guifont='Monaspace Neon Regular:h12'
 	vim.g.neovide_transparency = 0.8
 	vim.g.neovide_refresh_rate = 60
 	local system = vim.loop.os_uname().sysname
 	if system == 'Darwin' then 
+		vim.o.guifont='Monaspace Neon Medium:h12'
 		vim.g.neovide_window_blurred = true
 	end
 end
