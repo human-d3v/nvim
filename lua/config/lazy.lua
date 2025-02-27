81d9cd0ad3a6dc92bee4e820092e4ae083149e39
human-d3v <brytop05@gmail.com>
2025-02-27 11:31:33 -0700

    Added lspkind

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,6 +30,7 @@ local plugins = {
 	{"hrsh7th/cmp-nvim-lsp-signature-help"},
 	{"hrsh7th/cmp-path"},
 	{"hrsh7th/cmp-buffer"},
+	{"onsails/lspkind.nvim"},
 	{"L3MON4D3/LuaSnip", version  = 'v2.*', build = "make install_jsregexp",
 		dependencies = {'saadparwaiz1/cmp_luasnip'}},
 	{'williamboman/mason.nvim'},
