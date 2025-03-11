a713ecabc3347bba151376127bef6ba814ca32bb
human-d3v <brytop05@gmail.com>
2025-03-11 17:18:08 -0600

    Updated neovide for font size

--- after/plugin/neovide.lua
+++ after/plugin/neovide.lua
@@ -1,9 +1,9 @@
 if vim.g.neovide then 
-	vim.g.neovide_transparency = 0.6
+	vim.g.neovide_opacity = 0.6
 	vim.g.neovide_refresh_rate = 60
 	local system = vim.loop.os_uname().sysname
+	vim.o.guifont='Monaspace Neon Medium:h12'
 	if system == 'Darwin' then 
-		vim.o.guifont='Monaspace Neon Medium:h12'
 		vim.g.neovide_window_blurred = true
 	end
 end
