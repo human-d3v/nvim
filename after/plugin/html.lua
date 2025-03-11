17fabd82234a8c60de17f82a1a1ad3d6542d034f
human-d3v <brytop05@gmail.com>
2025-03-11 17:25:11 -0600

    Updated capabilites

--- after/plugin/html.lua
+++ after/plugin/html.lua
@@ -2,5 +2,10 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "html",
   callback = function()
 		require('luasnip.loaders.from_vscode').lazy_load()
+		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities.textDocument.completion.completionItem.snippetSupport = true 
+		require('lspconfig').html.setup{
+			capabilities = capabilities
+		}
   end,
 })
