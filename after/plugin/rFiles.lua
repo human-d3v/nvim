099937807de123e496a6f863f13766b01cbfbc00
human-d3v <brytop05@gmail.com>
2025-03-19 11:44:51 -0600

    Added additional cmp sources to R prog lang files

--- after/plugin/rFiles.lua
+++ after/plugin/rFiles.lua
@@ -5,7 +5,13 @@ vim.api.nvim_create_autocmd("FileType", {
 			vim.keymap.set('t','<leader>mv', ":lua ResizeAndMove(80)<CR>", {buffer = true})
 			vim.keymap.set("i", "<C-.>", " %>% ", {buffer = true})
 			vim.keymap.set("i", "<C-->", "<Plug>RInsertAssign", {buffer = true, silent = true}) -- reassign keymap for assignment
-			require('cmp').setup({sources = {{ name = "cmp_r" }}})
+			require('cmp').setup({sources = {
+				{ name = "cmp_r" },
+				{ name = "codeium" }, 
+				{ name = "luasnip" },
+				{ name = "nvim_lsp_signature_help" },
+				{ name = "buffer" },
+			}})
 			require('cmp_r').setup({})
 		end)
 	end
