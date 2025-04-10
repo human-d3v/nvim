ffebf0f63ebef21702db19e38f03944e50f76d92
human-d3v <brytop05@gmail.com>
2025-04-10 10:23:53 -0600

    Cleaned up lazy import file

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -55,13 +55,13 @@ local plugins = {
 	--Stata syntax highlighting
 	{'poliquin/stata-vim', ft = {'stata'}},
 	--ai auto completion with codeium
-	{"Exafunction/codeium.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"hrsh7th/nvim-cmp"},
-	   config = function()
-			require("codeium").setup({})
-	 end},
+	-- {"Exafunction/codeium.nvim",
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 		"hrsh7th/nvim-cmp"},
+	--    config = function()
+	-- 		require("codeium").setup({})
+	--  end},
 		--  {
 		--     "zbirenbaum/copilot.lua",
 		--     cmd = "Copilot",
@@ -109,8 +109,6 @@ local plugins = {
 			},
 		}
 	},
-	-- {"vhyrro/luarocks.nvim", priority = 1001, opts = { rocks = {"magick"}}}, -- not necessary for neovim 11+
-	-- {"3rd/image.nvim", dependencies = {"luarocks.nvim"}, config = function() end},
 	-- {
 	-- 	'human-d3v/stata-nvim', branch = 'main', ft = {'stata'},
 	-- 	build = 'git pull && cd lsp-server && npm init -y && npm install && bun build ./server/src/server.ts --compile --outfile server_bin && cd ..',
