1a363fbe71b810d9f71e567f93b2dfda71b32970
human-d3v <brytop05@gmail.com>
2025-08-25 11:00:40 -0600

    Updated diagnostic jumping

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -18,8 +18,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set('n','K',function() vim.lsp.buf.hover() end, opts) -- hover
 		vim.keymap.set('n','<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts) --view workspace
 		vim.keymap.set('n','<leader>vd', function() vim.diagnostic.open_float() end, opts) --view diagnostic
-		vim.keymap.set('n','[d',function() vim.diagnostic.goto_next({severity = {min = vim.diagnostic.severity.HINT}}) end, opts)
-		vim.keymap.set('n',']d',function() vim.diagnostic.goto_prev({severity = {min = vim.diagnostic.severity.HINT}}) end, opts)
+		vim.keymap.set('n','[d',function() vim.diagnostic.jump({count=1, float=true, severity = {min = vim.diagnostic.severity.HINT}}) end, opts)
+		vim.keymap.set('n',']d',function() vim.diagnostic.jump({count=-1, float=true, severity = {min = vim.diagnostic.severity.HINT}}) end, opts)
 		vim.keymap.set('n','<leader>vca', function() vim.lsp.buf.code_action() end, opts) --view code action
 		vim.keymap.set('n','<leader>vrn', function() vim.lsp.buf.rename() end, opts) --rename variables
 		vim.keymap.set('n','<leader>vrr', function() vim.lsp.buf.references() end, opts)
