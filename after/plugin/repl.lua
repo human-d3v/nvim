3086cf4aff7b0801e27f5842cd4b9dc6984b69a2
human-d3v <brytop05@gmail.com>
2025-09-02 12:00:52 -0600

    (Fix): Buffer failed to render due to filetype auto_cmd

--- after/plugin/repl.lua
+++ after/plugin/repl.lua
@@ -58,26 +58,6 @@ local function terminal_linker_buf(term_id)
   vim.api.nvim_buf_set_keymap(term_buf, "n", "<C-c>", "<C-\\><C-n>:q!<CR>", { noremap = true, silent = true })
 end
 
-vim.api.nvim_create_autocmd("FileType",{
-	pattern = {
-		"python",
-		"stata"
-	},
-	callback = function ()
-		vim.schedule(function ()
-			local opts = {buffer = true, silent = true}
-			local toggleterm = require('toggleterm')
-			vim.keymap.set("n", "<Bslash>d", function ()
-				toggleterm.send_lines_to_terminal("single_line", false, {})
-			end, opts)
-			vim.keymap.set("v", "<Bslash>d", function ()
-				toggleterm.send_lines_to_terminal("visual_lines", false, {})
-			end, opts)
-		end)
-	end
-})
-
-
 vim.keymap.set("n", "<Bslash>t", function ()
 	local win = vim.api.nvim_get_current_win()
 	toggleterm.toggle(1) -- assuming terminal ID 1
@@ -90,3 +70,27 @@ vim.keymap.set("n", "<leader>t", function()
 	terminal_linker_buf()
 end, {})
 
+vim.api.nvim_create_autocmd("FileType",{
+	pattern = {
+		"python",
+		"stata"
+	},
+	callback = function ()
+		vim.schedule(function ()
+			local opts = {buffer = true, silent = true}
+			vim.keymap.set("n", "<Bslash>d", function ()
+				toggleterm.send_lines_to_terminal("single_line", false, {})
+			end, opts)
+			vim.keymap.set("v", "<Bslash>d", function ()
+				toggleterm.send_lines_to_terminal("visual_lines", false, {})
+			end, opts)
+			vim.keymap.set("n", "<Bslash>t", function ()
+				local win = vim.api.nvim_get_current_win()
+				toggleterm.toggle(1)
+				vim.schedule(function ()
+					vim.api.nvim_set_current_win(win)
+				end)
+			end, opts)
+		end)
+	end
+})
